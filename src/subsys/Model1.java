8cac1bd732007063400994cbf66f073723a8b3ca
sovietballoon <sovietballoon@gmail.com>
2014-07-29 07:31:11 +0600

    Update Model1.java

--- src/subsys/Model1.java
+++ src/subsys/Model1.java
@@ -34,8 +34,6 @@ private Config config;
 		double weight_difference = 0;
 		List<Coil> coils_list = new ArrayList<Coil>(coils.values());
 		List<Coil> coils_ost = new ArrayList<Coil>();
-		//TypeChanger tch = new TypeChanger();
-		String addittype = null;
 		boolean flag = false;
 		int n_valid_thick = 0, n =0, reg_1 = 0, reg_2 = 0, reg_3 = 0, reg_4 = 0, reg_5 = 0, kol = 0;
 		String qur_type = null;
@@ -128,30 +126,17 @@ private Config config;
 				}
 				else if (furn.size() == 0
 						&& coil.thick > config.FURN_ONE_COIL_THINK_MORE
-						//&& coil.use == 1
 						){
 					furn.add(coil);
 					furn_regim = coil.regim;
 					furn_height = coil.width;
 					weight_difference = coil.weight;
 					System.out.printf("prior " + coil.prior + " regim " + coil.regim + " thick_1 = " + coil.thick + " width " + coil.width + " weight " + coil.weight + " ");					
-					// конечно же, надо придумать более адекватную модель сравнения режимов 
-					//относительно данных в конфиге, но пока так
 					
+					qur_type = coil.regim;
+					add_type = config.tySort(qur_type);
 					
-					if (coil.regim.equals("1")) 
-						addittype = "2";
-						else if (coil.regim.equals("2"))
-							addittype = "3";
-							else if (coil.regim.equals("3"))
-								addittype = "4";
-								else if (coil.regim.equals("4"))
-									addittype = "5";
-									else if (coil.regim.equals("5"))
-										addittype = "5";
-					
-					
-				}else if((coil.regim.equals(furn_regim) ||  coil.regim.equals(addittype))
+				}else if((coil.regim.equals(furn_regim) ||  coil.regim.equals(add_type))
 						&& (furn_height + coil.width) < this.config.FURN_HEIGHT 
 						&& furn.size() < this.config.FURN_COIL_MAX
 						&& Math.abs(coil.weight - weight_difference) < config.FURN_WEIGHT_DIFF_MAX
