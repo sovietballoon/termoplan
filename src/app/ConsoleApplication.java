6b0896287af3d0f60d1115dbfe05530bba2b3920
sovietballoon <sovietballoon@gmail.com>
2014-09-16 06:12:40 +0600

    Update ConsoleApplication.java

--- src/app/ConsoleApplication.java
+++ src/app/ConsoleApplication.java
@@ -2,6 +2,7 @@ package app;
 
 import java.util.*;
 import java.util.logging.*;
+
 import subsys.*;
 
 public class ConsoleApplication {
@@ -16,40 +17,50 @@ public class ConsoleApplication {
 			// Init Config
 			Config config = new Config();
 			// Load coils
-			coils = LoadCSV.load(config.DATAFILE, config.DATAFILE_SPLIT, config.DATEFORMAT);
+			coils = LoadCSV.load(Config.DATAFILE, Config.DATAFILE_SPLIT, Config.DATEFORMAT);
 			log.info("Load " + coils.size() + " coils");
 			
 			// init model
-			ModelSimple model1 = new ModelSimple(config);
-			Model1 model = new Model1(config);
-			List<List> output = model.calculate(coils);
-			
+			ModelSimple model = new ModelSimple(config);
+			Model model1 = new Model(config);
 			// Вывод результата, оценка алгоритма
-			String regim = "";
-			double kpd = 0;
-			double kpd_all = 0;
-			int coil_count = 0;
-			int width_sum = 0;
-			int width = 0;
 			
-			for (int i = 0; i < output.size(); i++){
-				List<Coil> furn = output.get(i);
-				for(Coil c: furn){
-					width += c.width;
-					regim += c.regim + " ";
-					coil_count += 1;
-				}
-				kpd = (double)width / config.FURN_HEIGHT * 100;
-				System.out.printf("furn:%s kpd:%.1f%% width:%s regim:%s \n", (i + 1), kpd, width, regim);
-				width_sum += width;
-				width = 0;
-				regim = "";
-			}
-			kpd = (double)width_sum / (config.FURN_HEIGHT * output.size()) * 100;
-			System.out.printf("\nAll furns %s, All coils %s, All KPD %.1f %%\n", output.size(), coil_count, kpd);
-		}catch(Exception  e){
+			funcOutput(model.calculate(coils));
+			funcOutput(model1.calculate(coils));
+			}catch(Exception  e){
 			log.log(Level.SEVERE, "Exception: ", e);
+		}	
+	}
+
+	private static void funcOutput(List<List> calculate) {
+		List<List> output = (List<List>) calculate;
+		String regim = "";
+		double kpd = 0;
+		double kpd_all = 0;
+		int coil_count = 0;
+		int width_sum = 0;
+		int width = 0;
+		String thick = "";
+		
+		for (int i = 0; i < output.size(); i++){
+			List<Coil> furn = output.get(i);
+			//System.out.println(output.get(i));
+			for(Coil c: furn){
+				width += c.width;
+				regim += c.regim + " ";
+				thick += "("+ c.thick + ");";
+				coil_count += 1;
+			}
+			kpd = (double)width / Config.FURN_HEIGHT * 100;
+			System.out.printf("furn:%s kpd:%.1f%% width:%s regim:%s thick:%s \n", (i + 1), kpd, width, regim, thick);
+			width_sum += width;
+			width = 0;
+			regim = "";
+			thick = "";
 		}
+		kpd = (double)width_sum / (Config.FURN_HEIGHT * output.size()) * 100;
+		System.out.printf("\nAll furns %s, All coils %s, All KPD %.1f %%\n", output.size(), coil_count, kpd);
+		
 	}
 }
 
